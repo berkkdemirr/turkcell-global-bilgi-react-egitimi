a0355b547b5acffadc22811eff854ffd068d9b08
eminbasbayan <emin.basbayan@icloud.com>
2023-10-19 14:54:33 +0300

    react componentlerinde javascript dosya kullanımı

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,29 +1,6 @@
 import ProductItem from "./ProductItem";
+import { productData } from "../../productData";
 import "./Products.css";
-
-const productData = [
-  {
-    id: 1,
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyqlw759cys5to_58DDt2vgV5PafoDhBI6cQ&usqp=CAU",
-    productTitle: "Şapka",
-    productPrice: 80,
-  },
-  {
-    id: 2,
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdZRDVij3RuXMIOrulPp9Z7CsEryJV8yeCIg&usqp=CAU",
-    productTitle: "Tisört",
-    productPrice: 100,
-  },
-  {
-    id: 3,
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjkbspNzN2RDUsGzYtfubI6_Vp6EshTlSJJA&usqp=CAU",
-    productTitle: "Kamera",
-    productPrice: 3000,
-  },
-];
 
 function Products(props) {
   return (
---
+++ src/productData.js
@@ -0,0 +1,24 @@
+export const productData = [
+    {
+      id: 1,
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyqlw759cys5to_58DDt2vgV5PafoDhBI6cQ&usqp=CAU",
+      productTitle: "Şapka",
+      productPrice: 80,
+    },
+    {
+      id: 2,
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdZRDVij3RuXMIOrulPp9Z7CsEryJV8yeCIg&usqp=CAU",
+      productTitle: "Tisört",
+      productPrice: 100,
+    },
+    {
+      id: 3,
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjkbspNzN2RDUsGzYtfubI6_Vp6EshTlSJJA&usqp=CAU",
+      productTitle: "Kamera",
+      productPrice: 3000,
+    },
+  ];
+  
