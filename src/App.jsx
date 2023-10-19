46750df9751601046a42408a165e4c2beff0dd55
eminbasbayan <emin.basbayan@icloud.com>
2023-10-19 16:24:49 +0300

    state uygulama

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
-import { useState } from "react";
+import Products from "./components/Products/Products";
 
 function App() {
-  const [name, setName] = useState("Emin");
 
-  // let name = "Emin";
-
-  function handleNameChange() {
-    // name = "Ali";
-    // console.log(name);
-    setName("Ali")
-  }
   return (
     <div className="app">
-      <h1 className="name">{name}</h1>
-      <button onClick={handleNameChange}>Title Change</button>
+     <Products />
     </div>
   );
 }
--- src/components/Products/ProductItem.jsx
+++ src/components/Products/ProductItem.jsx
@@ -1,17 +1,29 @@
+import { useState } from "react";
 import Button from "../UI/Button";
 import ProductInfo from "./ProductInfo";
 import "./ProductItem.css";
 
 function ProductItem(props) {
+  // let productTitle = props.item.productTitle;
+  const [productTitle, setProductTitle] = useState(props.item.productTitle)
+
+  function handleTitleChange() {
+    // productTitle = "Değiştirildi";
+    // console.log(productTitle);
+    setProductTitle(`${productTitle} Değiştirildi`)
+  }
+
+  console.log("re-rendered");
+
   return (
     <div className="product-item">
       <img src={props.item.imageUrl} alt="soda" className="product-image" />
       <div className="product-bottom">
         <ProductInfo>
-          <b className="product-title">{props.item.productTitle}</b>
+          <b className="product-title">{productTitle}</b>
           <span className="product-price">{props.item.productPrice}₺</span>
         </ProductInfo>
-        <Button title="Sepete Ekle" />
+        <Button title="Ürün Adı Değiştir" onClick={handleTitleChange} />
       </div>
     </div>
   );
