c877ec727f5c1287d5df5c8d690719f0b2a70efb
eminbasbayan <emin.basbayan@icloud.com>
2023-10-20 09:51:41 +0300

    useState counter uygulama

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Counter from "./components/Counter";
 import Products from "./components/Products/Products";
 
 function App() {
-
   return (
     <div className="app">
-     <Products />
+      <Counter />
+      <Products />
     </div>
   );
 }
---
+++ src/components/Counter.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function Counter() {
+  const [counter, setCounter] = useState(0);
+  return (
+    <div>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
+      <span> {counter} </span>
+      <button onClick={() => setCounter(counter - 1)}>-</button>
+    </div>
+  );
+}
+
+export default Counter;
