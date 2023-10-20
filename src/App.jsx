d1bc1e1fbf3037efee57f8d9b918032d9a943095
eminbasbayan <emin.basbayan@icloud.com>
2023-10-20 14:41:50 +0300

    useEffect hook tanımlama

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Counter from "./components/Counter";
 import ProductForm from "./components/Products/ProductForm";
 import Products from "./components/Products/Products";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [name, setName] = useState("Emin");
+
+  useEffect(() => {
+    console.log("bir kez çalıştı");
+  }, [counter]);
+
   return (
     <div className="app">
-      
-      {/* <Counter setCounter={setCounter}  /> */}
-      {/* {counter} */}
-      <Products />
+      {/* <Products /> */}
+      {counter}  <br />
+      <button onClick={() => setCounter(counter + 1)}>Arttır</button>
+      <br />
+      {name}  <br />
+      <button onClick={() => setName("Ahmet")}>İsmi Değiştir</button>
     </div>
   );
 }
