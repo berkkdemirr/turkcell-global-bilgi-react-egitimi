d188208f0a5fb96d2f49c4c3ed9a90bd8ac6ad28
eminbasbayan <emin.basbayan@icloud.com>
2023-10-20 10:03:25 +0300

    single state tanımlama

--- src/components/Products/ProductItem.jsx
+++ src/components/Products/ProductItem.jsx
@@ -2,15 +2,16 @@ import { useState } from "react";
 import Button from "../UI/Button";
 import ProductInfo from "./ProductInfo";
 import "./ProductItem.css";
+import Counter from "../Counter";
 
 function ProductItem(props) {
   // let productTitle = props.item.productTitle;
-  const [productTitle, setProductTitle] = useState(props.item.productTitle)
+  const [productTitle, setProductTitle] = useState(props.item.productTitle);
 
   function handleTitleChange() {
     // productTitle = "Değiştirildi";
     // console.log(productTitle);
-    setProductTitle(`${productTitle} Değiştirildi`)
+    setProductTitle(`${productTitle} Değiştirildi`);
   }
 
   console.log("re-rendered");
@@ -22,6 +23,7 @@ function ProductItem(props) {
         <ProductInfo>
           <b className="product-title">{productTitle}</b>
           <span className="product-price">{props.item.productPrice}₺</span>
+          <Counter />
         </ProductInfo>
         <Button title="Ürün Adı Değiştir" onClick={handleTitleChange} />
       </div>
