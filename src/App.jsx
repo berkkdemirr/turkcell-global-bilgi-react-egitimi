1053c2d54a0efaef4e2f98e8ec3d1f3ccf54cd3d
eminbasbayan <emin.basbayan@icloud.com>
2023-10-20 15:41:47 +0300

    react fetch işlemleri

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,50 @@
 import { useState } from "react";
-import Modal from "./components/UI/Modal";
+import Button from "./components/UI/Button";
+import { useEffect } from "react";
 
 function App() {
-  const [isShowModal, setIsShowModal] = useState(false);
+  const [count, setCount] = useState(0);
+  async function getUsers() {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+      }
+    } catch (err) {
+      console.log(err);
+    } finally {
+      console.log("işlem tamamlandı");
+    }
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
     <div className="app">
-      {isShowModal && <Modal setIsShowModal={setIsShowModal} />}
-      <br /><br /><br /><br />
-      <button onClick={() => setIsShowModal(true)}>Modal Aç</button>
+      <Button title="Kullanıcıları Getir" onClick={getUsers} />
+      <button onClick={() => setCount(count + 1)}>Arttır</button>
     </div>
   );
 }
 
 export default App;
+
+// function getUsers() {
+//   fetch("https://jsonplaceholder.typicode.com/users")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       const findUser = data.filter((user) => user.username.length > 8);
+//       console.log(findUser);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     })
+//     .finally(() => {
+//       console.log("işlem tamamlandı");
+//     });
+// }
