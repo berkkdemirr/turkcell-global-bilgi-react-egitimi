ea9185804aa9f79c59dd4112191841da47740494
eminbasbayan <emin.basbayan@icloud.com>
2023-10-20 11:52:26 +0300

    previous state kullanımı

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import Counter from "./components/Counter";
 import ProductForm from "./components/Products/ProductForm";
 import Products from "./components/Products/Products";
 
 function App() {
+  const [counter, setCounter] = useState(0);
   return (
     <div className="app">
       <ProductForm />
-      {/* <Counter />
-      <Products /> */}
+      <Counter setCounter={setCounter}  />
+      {counter}
+      {/* <Products /> */}
     </div>
   );
 }
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,12 +1,14 @@
-import { useState } from "react";
-
-function Counter() {
-  const [counter, setCounter] = useState(0);
+/* Previous State */
+function Counter(props) {
   return (
     <div>
-      <button onClick={() => setCounter(counter + 1)}>+</button>
-      <span> {counter} </span>
-      <button onClick={() => setCounter(counter - 1)}>-</button>
+      <button onClick={() => props.setCounter((prevState) => prevState + 1)}>
+        +
+      </button>
+      {/* <span> {props.counter} </span> */}
+      <button onClick={() => props.setCounter((prevState) => prevState - 1)}>
+        -
+      </button>
     </div>
   );
 }
