9546d381faf1724582d361d739ef67327376eb7a
eminbasbayan <emin.basbayan@icloud.com>
2023-10-20 15:02:57 +0300

    cleanup function ve component unmounting

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,13 @@
-import { useState, useEffect } from "react";
-import Counter from "./components/Counter";
-import ProductForm from "./components/Products/ProductForm";
-import Products from "./components/Products/Products";
+import { useState } from "react";
+import Modal from "./components/UI/Modal";
 
 function App() {
-  const [counter, setCounter] = useState(
-    JSON.parse(localStorage.getItem("counter"))
-  );
-  const [name, setName] = useState("Emin");
-
-  useEffect(() => {
-    localStorage.setItem("counter", JSON.stringify(counter));
-  }, [counter]);
-
+  const [isShowModal, setIsShowModal] = useState(false);
   return (
     <div className="app">
-      {/* <Products /> */}
-      {counter} <br />
-      <button onClick={() => setCounter(counter + 1)}>Arttır</button>
-      <br />
-      {name} <br />
-      <button onClick={() => setName("Ahmet")}>İsmi Değiştir</button>
+      {isShowModal && <Modal setIsShowModal={setIsShowModal} />}
+      <br /><br /><br /><br />
+      <button onClick={() => setIsShowModal(true)}>Modal Aç</button>
     </div>
   );
 }
--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
+
 const Modal = (props) => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((prevCount) => prevCount + 1);
+    }, 1000);
+    console.log(count);
+    /* cleanup function -  Component Unmounting */
+    return () => {
+      clearInterval(interval);
+    };
+  }, [count]);
+
+
+
   return (
     <div>
       <h2>Input boş geçilemez</h2>
+      <h3>{count}</h3>
       <button onClick={() => props.setIsShowModal(false)}>Modalı Kapat</button>
     </div>
   );
