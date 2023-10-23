68567bcdc3acf706631f9aa85f61c24ab45f6cb0
eminbasbayan <emin.basbayan@icloud.com>
2023-10-23 10:05:36 +0300

    ikinci route tanımlama

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import Products from "./components/Products/Products";
 import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
-  {path: "/", element: <HomePage />}
+  {path: "/", element: <HomePage />},
+  {path: "/products", element: <ProductsPage />}
 ])
 
 function App() {
