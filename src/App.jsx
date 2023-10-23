519199463bbbf24e2d4b5ce8c8f21c51041231c0
eminbasbayan <emin.basbayan@icloud.com>
2023-10-23 10:02:20 +0300

    react router ilk sayfa oluşturma

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import Products from "./components/Products/Products";
+import HomePage from "./pages/Home";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {path: "/", element: <HomePage />}
+])
 
 function App() {
   return (
     <div className="app">
-      <Products />
+       <RouterProvider router={router} />
     </div>
   );
 }
