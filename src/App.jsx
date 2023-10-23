fa00610f6613cb19efa3368c972c79b0df04e961
eminbasbayan <emin.basbayan@icloud.com>
2023-10-23 10:13:46 +0300

    route tanımlamanın alternatif yolu

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 
-const router = createBrowserRouter([
-  {path: "/", element: <HomePage />},
-  {path: "/products", element: <ProductsPage />}
-])
+const routerDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/products" element={<ProductsPage />} />
+  </Route>
+);
+
+const router = createBrowserRouter(routerDefinitions)
+
+// const router = createBrowserRouter([
+//   { path: "/", element: <HomePage /> },
+//   { path: "/products", element: <ProductsPage /> },
+// ]);
 
 function App() {
   return (
     <div className="app">
-       <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </div>
   );
 }
