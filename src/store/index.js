f62f41dd23f2100ef6c5b012eff57bee35bda5a6
eminbasbayan <emin.basbayan@icloud.com>
2023-10-23 16:03:19 +0300

    redux-toolkit yeni slice oluşturma

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,26 @@
-import { createStore } from "redux";
+// import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = { counter: 0, showCounter: false };
+
+createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
 const counterReducer = (state = { counter: 0, isShow: false }, action) => {
   if (action.type === "INCREMENT") {
