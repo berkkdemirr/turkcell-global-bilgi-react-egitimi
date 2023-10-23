f2df56403a29f4e4f4780b1437b45f87c186f920
eminbasbayan <emin.basbayan@icloud.com>
2023-10-23 13:38:19 +0300

    loading state

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -3,9 +3,11 @@ import { useParams } from "react-router-dom";
 
 const ProductDetailPage = () => {
   const [productDetail, setProductDetail] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
   const params = useParams();
 
   useEffect(() => {
+    setIsLoading(true);
     async function fetchSingleProduct() {
       try {
         const res = await fetch(
@@ -14,25 +16,35 @@ const ProductDetailPage = () => {
 
         if (res.ok) {
           const data = await res.json();
-          setProductDetail(data)
+          setProductDetail(data);
         }
       } catch (error) {
         console.log(error);
+      } finally {
+        setIsLoading(false);
       }
     }
     fetchSingleProduct();
   }, [params]);
-
-  console.log(productDetail);
   return (
     <div>
-      <h1>ProductDetailPage</h1>
-      <p>
-        Ürün Id: <b>{params.productId}</b>{" "}
-      </p>
-      <br />
-      <h2>{productDetail.title}</h2>
-      <img src={productDetail.image} alt={productDetail.title} width={200} />
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : (
+        <>
+          <h1>ProductDetailPage</h1>
+          <p>
+            Ürün Id: <b>{params.productId}</b>{" "}
+          </p>
+          <br />
+          <h2>{productDetail.title}</h2>
+          <img
+            src={productDetail.image}
+            alt={productDetail.title}
+            width={200}
+          />
+        </>
+      )}
     </div>
   );
 };
